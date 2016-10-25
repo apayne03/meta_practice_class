7aedee808960cd0473173f3affa1ededc44d1fb1
Alyssa Payne <apayne03@uoguelph.ca>
2016-10-25 10:40:51 -0400

    Commit.

--- meta_practice_class_script.R
+++ meta_practice_class_script.R
@@ -22,7 +22,7 @@ ex1_results
 # variance of 1000 studies = var_obs (standard weighted variance)
 # variability expected due to random sampling = var_exp
 # per_error = var_ex/var_obs > 75% means no moderators (<75% means there's moderators)
-# LL and UL = meta-analytic confidence interval; makes sense to look at if there are no moderators; if you have moderators, it suggests there are more than one population correlation
+# LL and UL = meta-analytic confidence interval; makes sense to look at if there are no moderators; if you have moderators, it suggests there are more than one population correlation THUS then cred_UL and cred_LL is more informative
 # cred_LL and cred_UL = 80% credibility interval = suggest no moderators
 
 meta_plot_funnel(ex1_results)
@@ -38,3 +38,32 @@ View(ex2_data)
 ex2_results
 
 meta_plot_forest(ex2_results)
+
+ex3_data <- read_csv("meta_data_ex3.csv")
+View(ex3_data)
+
+ex3_results <- meta_bare_bones(ex3_data)
+ex3_results
+
+meta_plot_funnel(ex3_results)
+meta_plot_forest(ex3_results)
+# forest plot here reveals that all of the ones in Canada were underestimates; US were overestimates - suggests different population correlations by country
+
+ex3_canada = ex3_data %>% filter(country=="Canada")
+ex3_usa = ex3_data %>% filter(country=="United States")
+
+View(ex3_canada)
+View(ex3_usa)
+
+meta_bare_bones(ex3_canada)
+meta_bare_bones(ex3_usa)
+# notice that it appears that the moderator issue has been solved since neither have moderators right now - and credibility intervals don't even overlap (although they can hypothetically); in class he pointed out r_bar (meta-analytic correlation), per_error and credibility intervals
+
+ex3_results_canada <- meta_bare_bones(ex3_canada)
+ex3_results_usa <- meta_bare_bones(ex3_usa)
+
+meta_plot_forest(ex3_results_canada)
+meta_plot_forest(ex3_results_usa)
+
+meta_plot_funnel(ex3_results_canada)
+meta_plot_funnel(ex3_results_usa)
